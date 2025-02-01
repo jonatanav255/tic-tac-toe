43213e53f2e6f4dd7074a6aa5f9f197185323b42
Jonatan <jonatanav255@hotmail.com>
2025-02-01 17:46:17 -0600

    adding env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,17 @@
 // src/db/index.ts
+import { config } from 'dotenv';
 import { Client } from 'pg';
 
-// Configure the PostgreSQL client connection
+// Load environment variables from the .env file
+config();
+
+// Create a new PostgreSQL client using environment variables
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'jonatanav255',
-  password: 'jonatan123',
-  database: 'myworkdb'
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE
 });
 
 // Connect to the database
