2eecbc8a46e4a2718fcf9f609fd21ada56a6abf8
Jonatan <jonatanav255@hotmail.com>
2025-02-12 13:38:57 -0600

    removing the ssl

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,8 +7,8 @@ import { Client } from 'pg';
 // Load environment variables from .env if you want in dev:
 config({ path: path.resolve(__dirname, '../../.env') });
 
-const caCertPath = process.env.PG_SSL_CA_PATH || '/app/us-east-1-bundle.pem';
-const caCert = fs.readFileSync(caCertPath, 'utf8');
+// const caCertPath = process.env.PG_SSL_CA_PATH || '/app/us-east-1-bundle.pem';
+// const caCert = fs.readFileSync(caCertPath, 'utf8');
 
 const client = new Client({
   host: process.env.PG_HOST,
@@ -16,10 +16,10 @@ const client = new Client({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-  ssl: {
-    ca: caCert,
-    rejectUnauthorized: true, // Force certificate validation
-  },
+  // ssl: {
+  //   ca: caCert,
+  //   rejectUnauthorized: true, // Force certificate validation
+  // },
 });
 
 client.connect()
